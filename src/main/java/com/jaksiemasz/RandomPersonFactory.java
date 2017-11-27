5e0824bacd56c75a49e2fa9418a199a0ee5e0fd2
Jakub Kowalski <kubabartek1@op.pl>
2017-11-27 10:18:53 +0100

    add gitignore

---
+++ src/main/java/com/jaksiemasz/RandomPersonFactory.java
@@ -0,0 +1,60 @@
+package com.jaksiemasz;
+
+import com.google.common.collect.ComparisonChain;
+
+import java.io.IOException;
+import java.util.*;
+
+public class RandomPerson {
+
+    public Person createPerson() throws IOException {
+        Person person =  new Person(randomName(), randomSurname(), pseudoRandomMail());
+        return person;
+    }
+
+    private String randomName() {
+        Random generate = new Random();
+        String[] name = {"John", "Marcus", "Susan", "Henry", "Jacob", "Thomas", "Angela", "Eliot", "Sammy", "Johanos"};
+
+        return name[generate.nextInt(10)];
+    }
+
+    private String randomSurname() {
+        Random generate = new Random();
+        String[] name = {"Smith", "Merc", "Bean", "Beer", "Anderson" , "Janson", "Scala"};
+
+        return name[generate.nextInt(7)];
+    }
+
+    private String pseudoRandomMail() {
+        return randomName() + randomSurname().length() + "@" + randomSurname();
+    }
+
+    public void ListOfRandomPeople(int numberOfPeople) throws IOException {
+        RandomPerson randomPerson = new RandomPerson();
+
+        List<Person> personList = new ArrayList<>();
+
+        for (int i = 0; i < numberOfPeople; i++) {
+            personList.add(randomPerson.createPerson());
+        }
+
+        Collections.sort(personList, new LexicographicComparator());
+
+        for (int i = 0; i < personList.size(); i++) {
+            personList.get(i).show();
+        }
+    }
+
+}
+
+class LexicographicComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person p1, Person p2) {
+        return ComparisonChain.start()
+                .compare(p1.getName(), p2.getName())
+                .compare(p1.getSurname(), p2.getSurname())
+                .compare(p1.getMail(), p2.getMail())
+                .result();
+    }
+}
