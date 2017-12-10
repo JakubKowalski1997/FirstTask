c6ae7c1d596c56b66ea852be2ee687b77fc0432e
jakub <kubabartek1@op.pl>
2017-12-10 18:04:49 +0100

    Task with random company done

--- src/main/java/com/jaksiemasz/Main.java
+++ src/main/java/com/jaksiemasz/Main.java
@@ -5,21 +5,12 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-       /*TeamMenager Grzesiek = new TeamMenager("Grzesiek",  10);
-       Grzesiek.hire(new Developer("Jakub Kowalski","Team leader"));
-        Grzesiek.hire(new Developer("Jakub Brzęczyszczykiewicz","Developer"));
-        Grzesiek.hire(new Developer("Jakub","Ko"));
-       Grzesiek.assign(new Task("123", 10,0));
-       Grzesiek.assign(new Task("bug", 4,1));
-       //System.out.println(Grzesiek.printListEmployee()); // nie działa jak chce :(
-       System.out.println(Grzesiek.getListEmployee().get(0).getRole());
-       System.out.println(Grzesiek.getListEmployee().get(1).getRole());
-       System.out.println(Grzesiek.getListEmployee().get(2).getRole());
-        System.out.println(Grzesiek.getListEmployee().get(0).reportWork());*/
 
-        Company comp1 = new Company(3);
+        Company comp1 = new CompanyFactory().getRandomCompany(4,"JakSieMasz.Care");
+        comp1.getCeoDirector().toString();
 
-        comp1.createCompany();
+
+
 
     }
 
